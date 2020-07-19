0bcf68c41da82ad0b24398dfa4d57a20829ecbc0
liujingwei05 <liujingwei05@meituan.com>
2020-07-19 17:35:44 +0800

    项目初始提交

---
+++ darren-dmall-product/src/main/java/com/darren/dmall/product/DarrenDmallProductApplication.java
@@ -0,0 +1,13 @@
+package com.darren.dmall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarrenDmallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarrenDmallProductApplication.class, args);
+    }
+
+}
---
+++ darren-dmall-product/src/test/java/com/darren/dmall/product/DarrenDmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darren.dmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarrenDmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
